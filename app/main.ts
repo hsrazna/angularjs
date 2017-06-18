537b42f4ae681140c6cc513dd37e68b4da346e74
hsrazna <anzar.terek@gmail.com>
2017-06-18 23:52:47 +0300

    first commit

---
+++ app/main.ts
@@ -0,0 +1,5 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { HelloAngularAppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(HelloAngularAppModule);
